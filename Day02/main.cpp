77735e38693c5f7feab1aaeb87c0ee4c47df124d
Ambroise Petitgenêt <apetitgenet@free.fr>
2023-12-05 18:52:21 +0000

    Day 02 part 2

--- Day02/main.cpp
+++ Day02/main.cpp
@@ -121,13 +121,83 @@ unsigned long treatPartOne()
 
 unsigned long treatPartTwo()
 {
+    std::vector<Game> games;
     std::string line;
     std::ifstream infile("input.txt");
     unsigned long result = 0;
     while (std::getline(infile, line))
     {
-       
+        Game game{};
+        std::uint64_t iLoop{0};
+        std::uint64_t startId{0};
+        std::uint64_t endId{0};
+        while(!isdigit(line[iLoop]))
+        {
+            iLoop++;
+        }
+        startId = iLoop;
+
+        while(isdigit(line[iLoop]))
+        {
+            iLoop++;
+        }
+        endId = iLoop;
+        const auto gameId{line.substr(startId, endId - startId)};
+        game.numero = std::stoull(gameId);
         
+
+        auto strBalls = line.substr(endId + 1);
+
+        std::vector<std::string> sets;
+        std::string parsed{};
+        std::stringstream ss{strBalls};
+        while (std::getline(ss, parsed,';'))
+        {
+            sets.push_back(parsed);
+        }
+
+        for(const auto set : sets)
+        {
+            std::stringstream sssets{set};
+
+            std::vector<std::string> cubes;
+            while (std::getline(sssets, parsed, ','))
+            {
+                cubes.push_back(parsed);
+            }
+
+            for(auto cube : cubes)
+            {
+                std::stringstream sscubes{cube};
+                std::vector<std::string> balls;
+                while (std::getline(sscubes, parsed, ' '))
+                {
+                    balls.push_back(parsed);
+                }
+
+                if(balls[2][0] == 'r')
+                {
+                    game.maxRed = std::max(game.maxRed, std::stoul(balls[1]));
+                }
+                else if(balls[2][0] == 'g')
+                {
+                    game.maxGreen = std::max(game.maxGreen, std::stoul(balls[1]));
+                }
+                else if(balls[2][0] == 'b')
+                {
+                    game.maxBlue = std::max(game.maxBlue, std::stoul(balls[1]));
+                }
+            }
+        }
+      
+        games.push_back(game);
+        
+    };
+
+    for(const auto& game : games)
+    {
+        int power = game.maxRed * game.maxGreen * game.maxBlue;
+        result += power;
     }
     return result;
 }
